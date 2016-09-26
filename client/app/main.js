a66fca464017392493bc415a37ca2bb315e4efad
Alex Horton <alexhortonmusic@gmail.com>
2016-09-26 15:50:37 -0500

    app reads and creates tasks

--- client/app/main.js
+++ client/app/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+angular
+  .module('toDo', ['ngRoute'])
+  .config($routeProvider =>
+    $routeProvider
+      .when('/', {
+        controller: 'ListCtrl',
+        templateUrl: 'partials/list.html'
+      })
+  )
+  .controller('ListCtrl', function ($scope, $http) {
+    $scope.addTask = () => {
+      const task = {
+        task: $scope.thingToDo
+      }
+      $scope.thingToDo = '' // empties input
+      $http
+        .post('/api/tasks', task)
+        .then(() => {
+          $scope.tasks.push(task)
+        })
+        .catch(console.error)
+    }
+    $http
+      .get('/api/tasks')
+      .then(({ data: { tasks }}) =>
+        $scope.tasks = tasks
+      )
+  })
--- server/server.js
+++ server/server.js
@@ -12,7 +12,31 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('client'))
 app.use(json())
 
+// routing
+app.get('/api/title', (req, res) => {
+  res.json({ title: 'To Do List'})
+})
 
+const Task = mongoose.model('task', {
+  task: String
+})
+
+app.get('/api/tasks', (req, res, err) => {
+  Task
+    .find()
+    .then(tasks => res.json({ tasks }))
+    .catch(err)
+})
+
+app.post('/api/tasks', (req, res) => {
+  const thingToDo = req.body
+  Task
+    .create(thingToDo)
+    .then(thingToDo => res.json(thingToDo))
+    .catch(console.error)
+})
+
+mongoose.Promise = Promise
 mongoose.connect(MONGODB_URL, () =>
   app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 )
